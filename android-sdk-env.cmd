6621b653c1d7bfed2759f3614d535a2cb3903df8
f3d3r1c0 <federico.montini@farmadati.it>
2015-11-21 16:13:12 +0100

    environment script for building with android sdk

---
+++ android-sdk-env.cmd
@@ -0,0 +1,8 @@
+@echo off
+SET ANDROID_HOME=C:\Users\federico.PCFARMADATI\AppData\Local\Android\android-sdk
+SET ANDROID_NDK=C:\Users\federico.PCFARMADATI\Documents\Android\ndk\android-ndk-r10d
+SET ANDROID_NDK_PATH=C:\Users\federico.PCFARMADATI\Documents\Android\ndk\android-ndk-r10d
+SET ANDROID_PLATFORM_TOOLS=C:\Users\federico.PCFARMADATI\AppData\Local\Android\android-sdk\platform-tools
+SET ANDROID_SDK=C:\Users\federico.PCFARMADATI\AppData\Local\Android\android-sdk
+SET PATH=%PATH%;%ANDROID_HOME%/tools;%ANDROID_PLATFORM_TOOLS%
+@echo Andorid SDK Environment set.
